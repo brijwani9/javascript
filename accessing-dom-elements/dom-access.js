10e6186110498d4fff6339d9e9b2f8314f04117e
brijwani9 <96011733+brijwani9@users.noreply.github.com>
2021-12-13 17:51:06 +0530

    Add files via upload

--- accessing-dom-elements/dom-access.js
+++ accessing-dom-elements/dom-access.js
@@ -1,10 +1,26 @@
 'use strict';
 
+// Access element with some class attached with it.
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = "Updated value for message with class  message."
 
+// Access some element from DOM with specific id.
 console.log(document.querySelector('#message').textContent);
 document.querySelector('#message').textContent = "Updated value for message with id  message."
 
+//Set value to input field of type text.
 document.querySelector('.inputText').value = "Input value";
 console.log(document.querySelector('.inputText').value);
+
+// Add click event listener on specific element.
+document.querySelector('.show_alert_button').addEventListener('click', () => {
+    alert("Input value is: " + document.querySelector('.inputText').value);
+    document.querySelector('#button_click_text').style.backgroundColor = 'Green';
+    document.querySelector('.final_message').classList.remove('final_message_red_class');
+    document.querySelector('.final_message').classList.add('final_message_green_class');
+});
+
+//Add keydown event listener on the whole document
+document.addEventListener('keydown', (event) => {
+    document.querySelector('.key_press_message').textContent = `${event.key} pressed.`;
+});
