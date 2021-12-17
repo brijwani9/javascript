5cd9a13b1f87690b21dbb972486ea839d986d9f0
brijwani9 <96011733+brijwani9@users.noreply.github.com>
2021-12-17 11:48:32 +0530

    Add files via upload

---
+++ math-rounding/math-rounding.js
@@ -0,0 +1,26 @@
+'use strict';
+
+console.log(Math.sqrt(25)); // 5
+
+console.log(Math.max(1, 2, 3, '4', 5, '6')); // 6
+console.log(Math.min('1', 2, 3, '4', 5, '6')); // 1
+
+console.log(Math.random()); // Generates some random number between 0 to 1
+
+console.log(Math.trunc(0.12)); // 0
+console.log(Math.trunc(25.23)); // 25
+
+console.log(Math.round(5.2)); // 5
+console.log(Math.round(5.9)); // 6
+
+console.log(Math.ceil(5.2)); // 6
+console.log(Math.ceil(5.9)); // 6
+
+console.log(Math.floor(5.2)); // 5
+console.log(Math.floor(5.9)); // 5
+
+console.log((2.2).toFixed(0)); // 2
+console.log((2.7).toFixed(0)); // 3
+
+console.log((2.2).toFixed(3)); // 2.200
+console.log((2.7).toFixed(5)); // 2.70000
