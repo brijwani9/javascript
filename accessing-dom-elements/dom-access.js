e66c09939593b239db87a76319b2ae3e67d5d235
brijwani9 <96011733+brijwani9@users.noreply.github.com>
2021-12-21 09:40:44 +0530

    Add files via upload

--- accessing-dom-elements/dom-access.js
+++ accessing-dom-elements/dom-access.js
@@ -24,3 +24,46 @@ document.querySelector('.show_alert_button').addEventListener('click', () => {
 document.addEventListener('keydown', (event) => {
     document.querySelector('.key_press_message').textContent = `${event.key} pressed.`;
 });
+
+// Access whole html. head and body
+console.log(document.documentElement); // Whole html
+console.log(document.head); // This will select only head part
+console.log(document.body); // This will select only body part
+
+// Get all elements by class section
+console.log(document.querySelectorAll('.section')); // This will return array of NodeList
+
+// Get all input tags
+console.log(document.getElementsByTagName('input')); // This will return array of HTMLCollection
+// The difference between NodeList & HTMLCollection is that, NodeList is not updated real time but HTMLCollection dose.
+
+// Creaing new elements
+const createdMessage = document.createElement('div');
+createdMessage.textContent = "This is programatically added message";
+
+// Both of the below lines will add createdMessage only once.
+// Because prepend and append methods will either add new element or move if already exists.
+// As a result we will see only element at the end of the document.
+document.body.prepend(createdMessage);
+document.body.append(createdMessage);
+
+// If we need element to be added twice then we can use something like below.
+const clonedExampleMessage = document.createElement('div');
+clonedExampleMessage.textContent = "This will appear twice in the DOM";
+document.body.prepend(clonedExampleMessage);
+document.body.append(clonedExampleMessage.cloneNode(true));
+
+// Before after examples
+const beforeAfterExample = document.createElement('div');
+beforeAfterExample.textContent = "BEFORE AFTER EXAMPLES";
+document.body.before(beforeAfterExample);
+document.body.after(beforeAfterExample.cloneNode(true));
+
+// Delete element from DOM
+const deleteExample = document.createElement('div');
+deleteExample.textContent = "THIS WILL BE REMOVED AFTER 3 SECONDS";
+document.body.after(deleteExample);
+
+setTimeout(() => {
+    deleteExample.remove();
+}, 3000);
